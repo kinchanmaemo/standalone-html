6affc8b3d837e1559f2a55bf80ef4ecae9ca87a1
kinchanmaemo <kinchan.maemo@gmail.com>
2017-01-19 23:55:57 +0100

    Update standalone-html.js

--- standalone-html.js
+++ standalone-html.js
@@ -2,10 +2,10 @@ var fs = require('fs');
 var prog = require('commander');
 var path = require('path');
 var cheerio = require("cheerio");
-var cssB64 = require('css-b64-images-no-limit');
+var cssB64 = require('css-b64-images');
 var colors = require('colors');
 
-var _contentTypes = {
+var imageTypes = {
 	".png": "image/png",
 	".gif": "image/gif",
 	".jpg": "image/jpeg",
@@ -14,52 +14,41 @@ var _contentTypes = {
 	".webp": "image/webp"
 }
 
-var standalone = function (inputPath, inputFile, outputPath) {
+var standalone = function (inputPath, inputFile, outputPath, writeFile) {
 	var $ = cheerio.load(inputFile);
 	$('html').find('link').each(function () {
 		if ($(this).attr('href')) {
 			var RawCssPath = $(this).attr('href');
 			var RawCssType = $(this).attr('type') ? $(this).attr('type') : 'undefined';
-			//console.log($(this).attr('type'))
 			var csspath = path.join(inputPath, RawCssPath);
 			// check if not a web link
 			if (RawCssPath.slice(0, 4) !== "http" && path.extname(RawCssPath) === '.css') {
-				fs.stat(csspath, function (err, stat) {
-					if (err === null) {
-						$(this).remove();
-						console.log('css : '.red + csspath);
-						cssB64.fromString(csspath, path.resolve(path.dirname(csspath)), path.resolve(path.dirname(inputPath)), function (err, css) {
-							if (err) {
-								console.error(err);
-							} else {
-								$('html').find('head').last().append('<style>' + fs.readFileSync(csspath, 'utf-8') + '</style>');
-							}
-						});
-					} else if (err.code == 'ENOENT') {
-						console.log('File does not found.'.red + ' skip.');
-						$(this).remove();
-					} else {
-						console.log('File does not found.'.red + ' skip.');
-						$(this).remove();
-					}
-				});
+				if (fs.existsSync(csspath)) {
+					$(this).remove();
+					console.log('css : '.red + csspath);
+					cssB64.fromString(csspath, path.resolve(path.dirname(csspath)), path.resolve(path.dirname(inputPath)), function (err, css) {
+						if (err) {
+							console.error(err);
+						} else {
+							$('html').find('head').last().append('<style>' + fs.readFileSync(csspath, 'utf-8') + '</style>');
+						}
+					});
+				} else {
+					console.log('File does not found.'.red + ' skip.');
+					$(this).remove();
+				}
 			} else if (RawCssPath.slice(0, 4) !== "http" && RawCssType.slice(0, 5) === 'image') {
-				fs.stat(csspath, function (err, stat) {
-					if (err === null) {
-						console.log('link icon : '.cyan + csspath);
-						var imgpath = path.join(inputPath, RawCssPath);
-						var img = fs.readFileSync(imgpath);
-						var contentType = _contentTypes[path.extname(imgpath)] || 'image/png'
-						var dataUri = "data:" + contentType + ";base64," + img.toString("base64");
-						$(this).attr('href', dataUri);
-					} else if (err.code == 'ENOENT') {
-						console.log('File does not found.'.red + ' skip.');
-						$(this).remove();
-					} else {
-						console.log('File does not found.'.red + ' skip.');
-						$(this).remove();
-					}
-				});
+				if (fs.existsSync(csspath)) {
+					console.log('link icon : '.cyan + csspath);
+					var imgpath = path.join(inputPath, RawCssPath);
+					var img = fs.readFileSync(imgpath);
+					var contentType = imageTypes[path.extname(imgpath)] || 'image/png'
+					var dataUri = "data:" + contentType + ";base64," + img.toString("base64");
+					$(this).attr('href', dataUri);
+				} else {
+					console.log('File does not found.'.red + ' skip.');
+					$(this).remove();
+				}
 			}
 		}
 	});
@@ -68,21 +57,16 @@ var standalone = function (inputPath, inputFile, outputPath) {
 		if ($(this).attr('src')) {
 			var RawImgPath = $(this).attr('src');
 			var imgpath = path.join(inputPath, RawImgPath);
-			fs.stat(imgpath, function (err, stat) {
-				if (err === null) {
-					var img = fs.readFileSync(imgpath)
-					var contentType = _contentTypes[path.extname(imgpath)] || 'image/png'
-					var dataUri = "data:" + contentType + ";base64," + img.toString("base64")
-					$(this).attr('src', dataUri)
-					console.log('img : '.yellow + imgpath);
-				} else if (err.code == 'ENOENT') {
-					console.log('File does not found.'.red + ' skip.');
-					$(this).remove();
-				} else {
-					console.log('File does not found.'.red + ' skip.');
-					$(this).remove();
-				}
-			});
+			if (fs.existsSync(imgpath)) {
+				var img = fs.readFileSync(imgpath);
+				var contentType = imageTypes[path.extname(imgpath)] || 'image/png';
+				var dataUri = "data:" + contentType + ";base64," + img.toString("base64");
+
+				$(this).attr('src', dataUri);
+				console.log('img : '.yellow + imgpath);
+			} else {
+				console.log('File does not found.'.red + ' skip.');
+			}
 		}
 	});
 
@@ -105,17 +89,7 @@ var standalone = function (inputPath, inputFile, outputPath) {
 			});
 		}
 	});
-
-	fs.writeFile(outputPath, $.html(), function (err) {
-		if (err) {
-			console.log('');
-			console.log('File error: ' + err + '. Exit.');
-		} else {
-			console.log('');
-			console.log('All done. Exit.'.green);
-		}
-	});
-
+	writeFile($.html(), outputPath);
 }
 
 module.exports = standalone;
